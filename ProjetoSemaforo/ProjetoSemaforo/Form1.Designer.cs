8d25a068b1415ef8cbc52b4e4d40a2d8066d02d5
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-11-23 21:14:27 -0300

    Add files via upload

---
+++ ProjetoSemaforo/ProjetoSemaforo/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace ProjetoSemaforo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLed1 = new System.Windows.Forms.Button();
+            this.btnLed2 = new System.Windows.Forms.Button();
+            this.btnLed3 = new System.Windows.Forms.Button();
+            this.btnLed4 = new System.Windows.Forms.Button();
+            this.btnLed5 = new System.Windows.Forms.Button();
+            this.btnLed6 = new System.Windows.Forms.Button();
+            this.btnLed7 = new System.Windows.Forms.Button();
+            this.btnLed8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLed1
+            // 
+            this.btnLed1.Location = new System.Drawing.Point(681, 368);
+            this.btnLed1.Name = "btnLed1";
+            this.btnLed1.Size = new System.Drawing.Size(75, 23);
+            this.btnLed1.TabIndex = 0;
+            this.btnLed1.Tag = "1";
+            this.btnLed1.Text = "ON/OFF";
+            this.btnLed1.UseVisualStyleBackColor = true;
+            this.btnLed1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLed1.ChangeUICues += new System.Windows.Forms.UICuesEventHandler(this.btnLed1_ChangeUICues);
+            // 
+            // btnLed2
+            // 
+            this.btnLed2.Location = new System.Drawing.Point(600, 368);
+            this.btnLed2.Name = "btnLed2";
+            this.btnLed2.Size = new System.Drawing.Size(75, 23);
+            this.btnLed2.TabIndex = 1;
+            this.btnLed2.Tag = "2";
+            this.btnLed2.Text = "ON/OFF";
+            this.btnLed2.UseVisualStyleBackColor = true;
+            this.btnLed2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed3
+            // 
+            this.btnLed3.Location = new System.Drawing.Point(519, 368);
+            this.btnLed3.Name = "btnLed3";
+            this.btnLed3.Size = new System.Drawing.Size(75, 23);
+            this.btnLed3.TabIndex = 2;
+            this.btnLed3.Tag = "3";
+            this.btnLed3.Text = "ON/OFF";
+            this.btnLed3.UseVisualStyleBackColor = true;
+            this.btnLed3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed4
+            // 
+            this.btnLed4.Location = new System.Drawing.Point(438, 368);
+            this.btnLed4.Name = "btnLed4";
+            this.btnLed4.Size = new System.Drawing.Size(75, 23);
+            this.btnLed4.TabIndex = 3;
+            this.btnLed4.Tag = "4";
+            this.btnLed4.Text = "ON/OFF";
+            this.btnLed4.UseVisualStyleBackColor = true;
+            this.btnLed4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed5
+            // 
+            this.btnLed5.Location = new System.Drawing.Point(357, 368);
+            this.btnLed5.Name = "btnLed5";
+            this.btnLed5.Size = new System.Drawing.Size(75, 23);
+            this.btnLed5.TabIndex = 4;
+            this.btnLed5.Tag = "5";
+            this.btnLed5.Text = "ON/OFF";
+            this.btnLed5.UseVisualStyleBackColor = true;
+            this.btnLed5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed6
+            // 
+            this.btnLed6.Location = new System.Drawing.Point(276, 368);
+            this.btnLed6.Name = "btnLed6";
+            this.btnLed6.Size = new System.Drawing.Size(75, 23);
+            this.btnLed6.TabIndex = 5;
+            this.btnLed6.Tag = "6";
+            this.btnLed6.Text = "ON/OFF";
+            this.btnLed6.UseVisualStyleBackColor = true;
+            this.btnLed6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed7
+            // 
+            this.btnLed7.Location = new System.Drawing.Point(195, 368);
+            this.btnLed7.Name = "btnLed7";
+            this.btnLed7.Size = new System.Drawing.Size(75, 23);
+            this.btnLed7.TabIndex = 6;
+            this.btnLed7.Tag = "7";
+            this.btnLed7.Text = "ON/OFF";
+            this.btnLed7.UseVisualStyleBackColor = true;
+            this.btnLed7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLed8
+            // 
+            this.btnLed8.Location = new System.Drawing.Point(114, 368);
+            this.btnLed8.Name = "btnLed8";
+            this.btnLed8.Size = new System.Drawing.Size(75, 23);
+            this.btnLed8.TabIndex = 7;
+            this.btnLed8.Tag = "8";
+            this.btnLed8.Text = "ON/OFF";
+            this.btnLed8.UseVisualStyleBackColor = true;
+            this.btnLed8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox9.Location = new System.Drawing.Point(195, 282);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 17;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox8.Location = new System.Drawing.Point(276, 282);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 16;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox7.Location = new System.Drawing.Point(357, 282);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox6.Location = new System.Drawing.Point(438, 282);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox5.Location = new System.Drawing.Point(519, 282);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox4.Location = new System.Drawing.Point(600, 282);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox3.Location = new System.Drawing.Point(681, 282);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoSemaforo.Properties.Resources.verm1;
+            this.pictureBox1.Location = new System.Drawing.Point(114, 282);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLed8);
+            this.Controls.Add(this.btnLed7);
+            this.Controls.Add(this.btnLed6);
+            this.Controls.Add(this.btnLed5);
+            this.Controls.Add(this.btnLed4);
+            this.Controls.Add(this.btnLed3);
+            this.Controls.Add(this.btnLed2);
+            this.Controls.Add(this.btnLed1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLed1;
+        private System.Windows.Forms.Button btnLed2;
+        private System.Windows.Forms.Button btnLed3;
+        private System.Windows.Forms.Button btnLed4;
+        private System.Windows.Forms.Button btnLed5;
+        private System.Windows.Forms.Button btnLed6;
+        private System.Windows.Forms.Button btnLed7;
+        private System.Windows.Forms.Button btnLed8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+    }
+}
+
---
+++ ProjetoSemaforo/ProjetoSemaforo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoSemaforo
+{
+    public partial class Form1 : Form
+    {
+        private Leds l;
+        public Form1()
+        {
+
+            InitializeComponent();
+            l = new Leds();
+            atualizaInterface();
+        }
+
+        private void atualizaInterface()
+        {
+            label1.Text = l.getEstado().ToString();
+            btnLed1.Text = (l.getEstado(1) ? "OFF" : "ON");
+            btnLed2.Text = (l.getEstado(2) ? "OFF" : "ON");
+            btnLed3.Text = (l.getEstado(3) ? "OFF" : "ON");
+            btnLed4.Text = (l.getEstado(4) ? "OFF" : "ON");
+            btnLed5.Text = (l.getEstado(5) ? "OFF" : "ON");
+            btnLed6.Text = (l.getEstado(6) ? "OFF" : "ON");
+            btnLed7.Text = (l.getEstado(7) ? "OFF" : "ON");
+            btnLed8.Text = (l.getEstado(8) ? "OFF" : "ON");
+            pictureBox3.Image = (l.getEstado(1)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox4.Image = (l.getEstado(2)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox5.Image = (l.getEstado(3)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox6.Image = (l.getEstado(4)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox7.Image = (l.getEstado(5)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox8.Image = (l.getEstado(6)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox9.Image = (l.getEstado(7)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+            pictureBox1.Image = (l.getEstado(8)) ? ProjetoSemaforo.Properties.Resources.verd :
+                  ProjetoSemaforo.Properties.Resources.verm1;
+  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int qualLed;
+
+            qualLed = int.Parse(((Button)sender).Tag.ToString());
+
+            if (l.getEstado(qualLed))
+                l.apagar(qualLed);
+            else
+                l.acender(qualLed);
+            atualizaInterface();
+
+
+        }
+
+        private void btnLed1_ChangeUICues(object sender, UICuesEventArgs e)
+        {
+
+        }
+    }
+}
